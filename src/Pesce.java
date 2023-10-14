3a2e3678e34c98ae52ffbea17c1aac1177bcd212
Marco <bertagnollimarco999@gmail.com>
2023-10-14 16:58:47 +0200

    classe Pesce: Costruttore

--- src/Pesce.java
+++ src/Pesce.java
@@ -4,5 +4,22 @@ public class Pesce {
     private int id;
     private String nome;
     private String colore;
-
+    
+    public Pesce(int id, String nome, String colore) throws InvalidParameterException {
+        if(this.id <= 0){
+            this.id = id;
+        } else {
+            throw new InvalidParameterException("id must be <= 0");
+        }
+        if(!this.nome.equals("")){
+            this.nome = nome;
+        } else {
+            throw new InvalidParameterException("nome must can't be empty");
+        }
+        if(!this.colore.equals("")){
+            this.colore = nome;
+        } else {
+            throw new InvalidParameterException("colore must can't be empty");
+        }
+    }
 }
