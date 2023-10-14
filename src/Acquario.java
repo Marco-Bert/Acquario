ca1e513511ea07e71156e51e10f719f1480d5385
Marco <bertagnollimarco999@gmail.com>
2023-10-14 17:01:10 +0200

    classe Acquario: Acquario(String nome, Pesce[] pesci)

--- src/Acquario.java
+++ src/Acquario.java
@@ -4,5 +4,13 @@ public class Acquario {
     public String nome;
     public Pesce[] pesci;
 
-
+    public Acquario(String nome, Pesce[] pesci) throws InvalidParameterException {
+        if(!this.nome.equals("")){
+            this.nome = nome;
+        } else {
+            throw new InvalidParameterException("nome must can't be empty");
+        }
+        this.pesci = new Pesce[pesci.length];
+        setPesci(pesci);
+    }
 }
